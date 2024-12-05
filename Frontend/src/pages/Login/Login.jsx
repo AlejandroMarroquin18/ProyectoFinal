c86fc2c7e6f257869ad6368a2d351bb792c41e4f
stiven-acua17 <brayan.acuna@correounivalle.edu.co>
2024-12-05 00:19:58 -0500

    HUF-20 #wip prueba despliegue

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -38,14 +38,6 @@ async function sendTokenToBackend(idToken) {
   try {
 
     const response = await request("/auth/validate-user", "POST", null, idToken)
-    /*const response = await fetch('http://localhost:3000/auth/validate-user', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${idToken}`,
-      },
-    });*/
-
     const data = await response.json();
     if (response.ok) {
       console.log('Token validado correctamente en el backend:', data);
@@ -72,6 +64,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const idToken = await loginUser(email, password);
+    
     if (idToken) {
       sendTokenToBackend(idToken);
       navigate("/home");
