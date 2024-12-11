6024a453372e747aca66c30f119f25dfff69947e
stiven-acua17 <brayan.acuna@correounivalle.edu.co>
2024-12-11 02:51:59 -0500

    intento puppeteer render

--- Backend/modules/scraping/scrapeTC.js
+++ Backend/modules/scraping/scrapeTC.js
@@ -29,7 +29,7 @@ const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', 
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
 
-    await page.goto(`https://tauretcomputadores.com/search_product?product_search=${search}`, {waitUntil: 'domcontentloaded'});
+    await page.goto(`https://tauretcomputadores.com/search_product?product_search=${search}`, {waitUntil: 'domcontentloaded', timeout: 20000});
     
     const products = await page.evaluate((amount) => {
       const productList = document.querySelectorAll('li .card');
@@ -66,6 +66,8 @@ const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', 
   }
 }
 
-
+/*(async() => {
+  await scrapeProductTC('table')
+})()*/
 
 module.exports = { scrapeProductTC }
